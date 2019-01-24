df0f5ba072905bd5ca4a7f814a672e1ca6a35d70
Bunlong <obunlong@gmail.com>
2019-01-24 09:11:14 +0700

    add webview page

--- src/navigation/screen.js
+++ src/navigation/screen.js
@@ -3,9 +3,11 @@ import { Provider }   from 'react-redux'
 import configureStore from '../redux/store'
 
 import SplashScreen   from '../screens/SplashScreen'
+import WebView        from '../screens/WebView'
 
 const store = configureStore()
 
 export default () => {
   Navigation.registerComponentWithRedux('oscar.splashScreen', () => SplashScreen, Provider, store)
+  Navigation.registerComponentWithRedux('oscar.WebView',      () => WebView, Provider, store)
 }
